062ec6e61fa08ffb44598a6544a7fcd9c5591f73
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-05-31 20:22:01 -0300

    Polybar things

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,9 +9,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Abrir polybar em todos os monitores
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    source $HOME/.env
     MONITOR=$m polybar --reload mainbar-i3 &
   done
 else
+  source $HOME/.env
   polybar --reload mainbar-i3 &
 fi
 
